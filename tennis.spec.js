7b2bffc0e7d2c6d2aa07dfe060216b040b4d8138
Pensupa Preechachat <pensupa47@gmail.com>
2017-09-02 12:14:16 +0700

    test tennis program playerA gets score pass

--- tennis.spec.js
+++ tennis.spec.js
@@ -1,40 +1,41 @@
 function TennisGame() {
-    this.reset = () => { //function ชื่อ reset
+    const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
+    this.playerAScore = 0
 
+    this.reset = () => {
+        this.playerAScore = 0
     }
 
-    this.echo = () => { //function ชื่อ echo
-      return 'Love-Love'
+    this.echo = () => {
+       return `${scoreString[this.playerAScore]} - Love`
     }
 
-    this.playerAGetScore = () => {
-      return 'fifteen-love'
+    this.playerAGteScore = () => {
+        this.playerAScore++
     }
 }
 
-test('Echo "Love-Love" when game start', () => {
-    //Arrange
+test('Echo "Love - Love" when game start', () => {
+    // Arrange
     let app = new TennisGame()
 
-    //Act
+    // Act
     app.reset()
     let result = app.echo()
 
-    //Assert
-    expect(result).toBe('Love-Love')
-
+    // Assert
+    expect(result).toBe('Love - Love')
 })
 
-test('Echo "fifteen-Love" when playerA gets first score', () => {
-  //Arrange
-  let app = new TennisGame()
-  app.reset()
+test('Echo "Fifteen - Love" when playerA get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerAGteScore()
 
-  //Act
-  app.playerAGetScore()
-  let result = app.echo()
+    // Act
+    let result = app.echo()
 
-  //Assert
-  expect(result).toBe('fifteen-Love')
-
+    // Assert
+    expect(result).toBe('Fifteen - Love')
 })
