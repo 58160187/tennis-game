73e1491c115bb9f4a6e77567f1461d501a39a5f5
Pensupa Preechachat <pensupa47@gmail.com>
2017-09-09 12:17:55 +0700

    learn function describe

--- tennis.spec.js
+++ tennis.spec.js
@@ -2,7 +2,6 @@ function TennisGame() {
     const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
     this.playerAScore = 0
     this.playerBScore = 0
-    this.playerAGetScoreThirty = 0
 
     this.reset = () => {
         this.playerAScore = 0
@@ -20,59 +19,67 @@ function TennisGame() {
     this.playerBGetScore = () => {
         this.playerBScore++
     }
-
-  
+    
 }
 
-test('Echo "Love - Love" when game start', () => {
-    // Arrange
-    let app = new TennisGame()
+describe ('echo()', () => {
+  let app //ประกาศตัวแปรไว้ใต้ฟังก์ชัน describe ฟังก์ชันทุกฟังก์ชันที่อยู่ใต้ฟังก์ชัน describe จะสามารถเรียกใช้ตัวแปรนี้ได้
 
-    // Act
+  beforeEach(() => {
+    app = new TennisGame()
     app.reset()
-    let result = app.echo()
+  })
 
-    // Assert
-    expect(result).toBe('Love - Love')
-})
+  test('Echo "Love - Love" when game start', () => {
+      // Arrange
+      //let app = new TennisGame()
 
-test('Echo "Fifteen - Love" when playerA get first score', () => {
-    // Arrange
-    let app = new TennisGame()
-    app.reset()
-    app.playerAGetScore()
+      // Act
+      //app.reset()
+      let result = app.echo()
 
-    // Act
-    let result = app.echo()
+      // Assert
+      expect(result).toBe('Love - Love')
+  })
 
-    // Assert
-    expect(result).toBe('Fifteen - Love')
-})
+  test('Echo "Fifteen - Love" when playerA get first score', () => {
+      // Arrange
+      //let app = new TennisGame()
+      //app.reset()
+      app.playerAGetScore()
 
-test('Echo "Love - Fifteen" when playerB get first score', () => {
-    // Arrange
-    let app = new TennisGame()
-    app.reset()
-    app.playerBGetScore()
+      // Act
+      let result = app.echo()
 
-    // Act
-    let result = app.echo()
+      // Assert
+      expect(result).toBe('Fifteen - Love')
+  })
 
-    // Assert
-    expect(result).toBe('Love - Fifteen')
-})
+  test('Echo "Love - Fifteen" when playerB get first score', () => {
+      // Arrange
+      //let app = new TennisGame()
+      //app.reset()
+      app.playerBGetScore()
 
-test('Echo "Thirty - Fifteen" when score is 30 - 15', () => {
-    // Arrange
-    let app = new TennisGame()
-    app.reset()
-    app.playerAGetScore()
-    app.playerBGetScore()
-    app.playerAGetScore()
+      // Act
+      let result = app.echo()
 
-    // Act
-    let result = app.echo()
+      // Assert
+      expect(result).toBe('Love - Fifteen')
+  })
 
-    // Assert
-    expect(result).toBe('Thirty - Fifteen')
+  test('Echo "Thirty - Fifteen" when score is 30 - 15', () => {
+      // Arrange
+      //let app = new TennisGame()
+      //app.reset()
+      app.playerAGetScore()
+      app.playerAGetScore()
+      app.playerBGetScore()
+
+      // Act
+      let result = app.echo()
+
+      // Assert
+      expect(result).toBe('Thirty - Fifteen')
+  })
 })
